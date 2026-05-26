5e69082774c45c44e23846f492df87fdaab6eca0
Poseidonsbreeze <chideraiwuchukwu@gmail.com>
2026-05-26 16:38:55 +0100

    requirements

---
+++ frontend/discrete.py
@@ -0,0 +1,29 @@
+
+
+# for i in range(7):
+#     for j in range(5):
+#         print(i,j)
+
+
+# num_students = int(input("Enter number of students:"))
+
+# for i in range(num_students):
+#     name = input("Enter student name:")
+    
+#     scores = []
+
+#     for j in range(3):
+#         score = int(input(f"Enter score for subject {j+1}:"))
+#         scores.append(score)
+
+# print(f"Student: {name}, Subject {j+1} Score: {score}")
+
+# def fib(n):
+#     if n <= 1:
+#         return n
+#     return fib(n-1) + fib(n-2)
+
+# n = int(input("Enter a number:"))
+# print(fib(n))
+
+fib(3)
